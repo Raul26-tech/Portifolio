46f9d3fffd9a62f0acbbcea968cceae0e9580e62
Raul26_tech <rauletec2020@gmail.com>
2023-02-25 19:59:57 -0300

    Finalizando Section

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -5,11 +5,19 @@ import Animation from '../Animation';
 export default function Layout() {
     return (
         <>
-            <div className="w-screen h-screen flex flex-col z-10 ">
+            <div className="w-full h-screen flex flex-col overflow-y-auto">
                 <Header />
-                <section className="w-full h-[30rem] flex bg-gradient-to-r from-gray-500 to-theme-blue-dark-gray-150">
+                <section className="w-full h-[15rem] flex flex-col bg-gradient-to-r from-gray-500 to-theme-blue-dark-gray-150">
                     <Animation />
                 </section>
+                <div className="bg-gradient-to-r from-gray-500 to-theme-blue-dark-gray-150">
+                    <img
+                        className="w-full"
+                        src="../src/assets/footerSection.png"
+                        alt=""
+                    />
+                </div>
+                <div className="bg-red-300 mt-3">Proxima sessão</div>
             </div>
         </>
     );
