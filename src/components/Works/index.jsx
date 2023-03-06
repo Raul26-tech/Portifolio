64a5ed42af6fb3ae19e4d389bef159af847c279c
Raul26_tech <rauletec2020@gmail.com>
2023-03-06 20:02:57 -0300

    Ajustando esperiências

--- src/components/Works/index.jsx
+++ src/components/Works/index.jsx
@@ -18,18 +18,41 @@ export default function Works() {
 
                     <div className="w-full md:flex justify-center items-start mt-10 p-3 rounded-md shadow-md bg-white">
                         <Experencies textButton="Asisto tecnologia">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Voluptate, illum saepe? Natus soluta a,
-                            repellendus asperiores eveniet, iste, culpa quae
-                            reiciendis laborum distinctio explicabo dolore sed
-                            quam magnam facilis iusto?
+                            <span className="text-lg font-bold">
+                                2022 - Atual
+                            </span>
+                            <p>
+                                Trabalho atuando como Desenvolvedor Front-end,
+                                desenvolvendo soluções para aplicações Web.
+                                <br />
+                                Atuando com diversas tecnologias com Html,css3,
+                                Tailwindcss, Javascript, React, Typescript.
+                                <br />
+                                Desde que adentrei tenho aprendido muito, não só
+                                os conhecimentos técnicos mas também, sobre o
+                                mercado e regra de negócio.
+                                <br /> Asisto é uma empresa de desenvolvimento
+                                de software especializada no ramo moveleiro.
+                                <br />
+                            </p>
                         </Experencies>
+
                         <Experencies textButton="Drean tecnology">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Voluptate, illum saepe? Natus soluta a,
-                            repellendus asperiores eveniet, iste, culpa quae
-                            reiciendis laborum distinctio explicabo dolore sed
-                            quam magnam facilis iusto?
+                            <span className="text-lg font-bold">
+                                2021 - Outubro - 2021
+                            </span>
+                            <p>
+                                Meu primeiro trabalho na área, onde atuei como
+                                desenvolvedor front-end freelance.
+                                <br />
+                                Trabalhei em alguns projetos onde tive o
+                                primeiro contato com o mundo do desenvolvimento.
+                                Onde realizava a manutenção de aplicações Web
+                                <br />
+                                Atuei com tecnologias como Html, Javascript,
+                                Styled-Component, css, React, React Native.
+                                <br />
+                            </p>
                         </Experencies>
                     </div>
                 </div>
