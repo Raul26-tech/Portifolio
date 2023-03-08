ab4c74412783375c601727b6f0bdcf00f0eeb666
Raul26_tech <rauletec2020@gmail.com>
2023-03-08 20:29:38 -0300

    Finalizando descrição dos projetos

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -69,26 +69,22 @@ export default function Projects() {
                         <div className="w-[15rem] md:w-[20rem] bg-slate-white rounded-md shadow-xl p-6 hover:-translate-y-2 hover:duration-300 cursor-pointer">
                             <div className="w-full h-20 flex justify-center items-center text-theme-blue text-base">
                                 <h2 className="text-2xl font-bold">
-                                    Nome proj
+                                    Netflix Clone
                                 </h2>
                             </div>
-                            <div className="w-full flex flex-col p-3 border-[1px] border-l-theme-blue rounded-md">
+                            <div className="w-full flex flex-col h-[20rem] p-3 border-[1px] border-l-theme-blue rounded-md">
                                 <p className="text-slate-600 text-sm">
-                                    Lorem ipsum dolor sit amet consectetur
-                                    adipisicing elit. Atque sed, dolorum dolore
-                                    incidunt nam est quia adipisci facere quasi
-                                    qui optio commodi expedita tempora vitae
-                                    reiciendis explicabo reprehenderit alias
-                                    sunt!
+                                    Clone da Netflix, coloco entre os principais
+                                    pois me deu a base de uma boa estrutura HTML
+                                    e conceitos importantes do CSS que utilizo
+                                    no dia-a-dia.
                                 </p>
                                 <div className="mt-2 flex flex-col">
                                     <span className="text-theme-blue text-base">
                                         Tecnologias utilizadas
                                     </span>
                                     <span className="text-slate-600 text-sm">
-                                        React, React-router-dom, react-icons,
-                                        react-hook-form, Typescript, Axios, Live
-                                        Server
+                                        HTML, CSS
                                     </span>
                                 </div>
                             </div>
